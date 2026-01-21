9c9f06fc78c69e1b5f6f1ee3f10d93b4f96735e4
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2026-01-21 21:02:44 +0200

    upload-chages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
 import store from './store/store.jsx';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/united-deals-website">
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
 )
 
